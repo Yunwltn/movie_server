3a2dbd4d5fc665c9aaee7f0bf3eb02fbd00d09f1
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2023-01-09 10:59:23 +0900

    리뷰 남기는 API

---
+++ resources/review.py
@@ -0,0 +1,44 @@
+from flask import request
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_restful import Resource
+from mysql_connection import get_connection
+from mysql.connector import Error
+
+class ReviewListResource(Resource) :
+
+    @jwt_required()
+    def post(self) :
+        # {"movie_id": "50",
+        # "rating": "1" }
+    
+        data = request.get_json()
+        user_id = get_jwt_identity()
+
+        try :
+            connection = get_connection()
+
+            query = '''insert into rating
+                    (user_id, movie_id, rating)
+                    values
+                    (%s, %s, %s);'''
+
+            record = ( user_id, data['movie_id'], data['rating'])
+
+            cursor = connection.cursor()
+
+            cursor.execute(query, record)
+
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+
+        except Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+
+            return {"result" : "fail", "error" : str(e)}, 500
+
+        return {"result" : "success"}, 200
+
